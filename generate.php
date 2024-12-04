20ae23e4d63fe491be5d750ebc7632d0f3cc40f2
abdullahi-maina <xboyabdul@gmail.com>
2024-12-04 11:48:29 +0100

    Update generate.php
    changed to hash passwords correctly

--- generate.php
+++ generate.php
@@ -1,6 +1,6 @@
 <?php
 // Replace 'your_plain_password' with the actual password you want to hash
-$plain_password = 'password123'; // Put the actual password here
+$plain_password = ''; // Put the actual password here
 $hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
 
 echo $hashed_password;
