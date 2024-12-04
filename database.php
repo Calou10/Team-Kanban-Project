1521793fd03e0bc87b580bf7cdf0ed659c2ca50e
abdullahi-maina <xboyabdul@gmail.com>
2024-12-04 11:47:18 +0100

    Update database.php
    database hostname changed to fit mysql container name in the phpmyadmin configuration file

--- database.php
+++ database.php
@@ -1,9 +1,10 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "test1";
+$hostName = "mysql-db"; // This is the service name defined in docker-compose.yml
+$dbUser = "user"; // MySQL user
+$dbPassword = "userpassword"; // MySQL user password
+$dbName = "test1"; // The database you want to use
+
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
